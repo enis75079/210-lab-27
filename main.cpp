541f80be7b08227d9854961056fc88ae795ac139
enis75079 <naveenislam55@gmail.com>
2024-11-03 20:25:58 -0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -12,12 +12,16 @@ Naveen Islam
 #include <string>
 using namespace std;
 
+// function prototypes
 void villagerOutput (const map<string, tuple<int, string, string>>&);
 
+// main function
 int main() {
-    // declarations
+    // creates a villager map
     map<string, tuple<int, string, string>> villager;
     int userChoice = 0;
+
+    // creates the variables that will be affected throughout the runtime of the while loop
     string name;
     string species;
     string catchphrase;
@@ -25,6 +29,7 @@ int main() {
 
 
     while (userChoice != 6) {
+        // menu that user can interact with
         cout << "1. Add Villager" << endl;
         cout << "2. Delete Villager" << endl;
         cout << "3. Increase Friendship" << endl;
@@ -36,6 +41,7 @@ int main() {
         cout << endl;
 
         if (userChoice == 1) {
+            // creates a new villager based on user input
             cout << "Villager's Name: ";
             cin.ignore();
             getline(cin, name);
@@ -48,11 +54,12 @@ int main() {
             cin.ignore();
 
             villager[name] = make_tuple(friendshipLvl, species, catchphrase);
-            cout << name << "added." << endl;
+            cout << name << " added." << endl;
 
         } else if (userChoice == 2) {
             cout << "Delete Villager: ";
             cin >> name;
+            // deletes the villager from the map
             if (villager.erase(name)) {
                 cout << "Villager " << name << " has been deleted." << endl; 
                 cout << endl;
@@ -65,24 +72,30 @@ int main() {
             cin >> name;
             auto it = villager.find(name);
             if (it != villager.end()) {
+                // if user chooses to increase friendship level
                 if (userChoice == 3) {
                     int& vLvl = get<0>(it->second);
                     if (vLvl < 10) {
+                        // increases the level by 1
                         vLvl++;
                         cout << "Your friendship with " << name << " has increased to " << vLvl << "." << endl;
                         cout << endl;
                     } else {
+                        // if the friendship level is higher than 10
                         cout << "You have reached maximum friendship points with this villager." << endl;
                         cout << endl;
                         
                     }
+                // if user chooses to increase friendship level
                 } else if (userChoice == 4) {
                     int& vLvl = get<0>(it->second);
                     if (vLvl > 0) {
+                        // decreases the level by 1
                         vLvl--;
                         cout << "Your friendship with " << name << " has decreased to " << vLvl << "." << endl;
                         cout << endl;
                     } else {
+                        // if the friendship level is lower than 0
                         cout << "You are at 0 friendship with this villager." << endl;
                         cout << endl;
                         
@@ -96,6 +109,7 @@ int main() {
             cout << "Search Villager: ";
             cin >> name;
             cout << endl;
+            // finds the villager with the user input name
             auto it = villager.find(name);
 
             if (it != villager.end()) {
@@ -106,9 +120,15 @@ int main() {
             }
         }
     }
+
+    // calls the villagerOutput function to display all the remaining villagers
+    villagerOutput(villager);
+    cout << endl;
+
     return 0;
 }
 
+// villagerOutput function. outputs the information of all the villagers after userChoice = 6 (exit)
 void villagerOutput (const map<string, tuple<int, string, string>>& villager) {
     cout <<"Villager details: " << endl;
     for (const auto& vProfile : villager) {
